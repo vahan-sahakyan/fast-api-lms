c86af9771aaea0137fe55bbc938f0ce52f7879a0
vahan-sahakyan <vahan.sahakyan1996@gmail.com>
2024-05-09 18:46:00 +0400

    db models and setup

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
-from api import users, courses, sections
+
+from api import courses, sections, users
+from db.db_setup import engine
+from db.models import course, user
+
+user.Base.metadata.create_all(bind=engine)
+course.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="Fast API LMS",
